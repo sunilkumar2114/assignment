e76f85183eebd0fb64f2945c884f18e1591c9eaf
sunilkumar <144036161+sunilkumar2114@users.noreply.github.com>
2023-09-11 12:16:00 +0530

    Add files via upload

---
+++ 11-09-2023/app4.py
@@ -0,0 +1,14 @@
+#my bio data
+name="sunilkumar"
+address="salem"
+total_sub="5"
+t=80
+e=82
+m=77
+sc=88
+s=92
+total=t+e+m+sc+s
+average=total/5
+print("My Name is",name,"My address is",address,"My total subject is",total_sub,"and My total Mark in 10th is",total,"My average is",average)
+
+
